b25b21f23a3f0e7eb2f3899a26732a59321217da
Shahmeer Sajid <118081563+ShahmeerSajid@users.noreply.github.com>
2026-07-15 17:54:07 +0500

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()){
+            return false;
+        }
+
+        int[] s1Frequency = new int[26];
+        int[] windowFrequency = new int[26];
+
+        // Compare the first window only right now ...
+        for (int right = 0; right < s1.length(); right++){
+            s1Frequency[s1.charAt(right) - 'a'] ++;
+            windowFrequency[s2.charAt(right) - 'a'] ++;
+        }
+
+        if (Arrays.equals(s1Frequency, windowFrequency))
+        {
+            return true;
+        }
+
+
+        // Now compare the other windows ...
+        for (int right = s1.length(); right<s2.length(); right++){
+
+            windowFrequency[s2.charAt(right) - 'a'] ++;
+
+            int left = right - s1.length();
+            windowFrequency[s2.charAt(left) - 'a']--;
+
+            if (Arrays.equals(s1Frequency, windowFrequency)){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
