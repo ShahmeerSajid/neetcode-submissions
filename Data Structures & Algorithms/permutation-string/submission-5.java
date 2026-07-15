478113a65235aa923b8bedeb4a74aab9d91d8a20
Shahmeer Sajid <118081563+ShahmeerSajid@users.noreply.github.com>
2026-07-15 18:01:09 +0500

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()){
+            return false;
+        }
+
+        int[] s1Frequency = new int[26];
+        int[] windowFrequency = new int[26];
+
+        // Compare the first window only right now ...
+        for (int right = 0; right < s1.length(); right++){
+            s1Frequency[s1.charAt(right) - 'a'] ++;
+            windowFrequency[s2.charAt(right) - 'a'] ++;
+        }
+
+        if (Arrays.equals(s1Frequency, windowFrequency))
+        {
+            return true;
+        }
+
+
+        // Now compare the other windows ...
+
+        // right pointer starts from s1.length();
+        for (int right = s1.length(); right<s2.length(); right++){
+
+            windowFrequency[s2.charAt(right) - 'a'] ++;
+
+            int left = right - s1.length(); // left pointer
+
+            windowFrequency[s2.charAt(left) - 'a']--;
+
+            if (Arrays.equals(s1Frequency, windowFrequency)){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
+
+// Time: O(n)
+// Space: O(1) --> We use 2 arrays and the size of the arrays is always 26.
+
+// s2 = l e c a b e e
+//      -----
+//       lec
+
+// s2 = l e c a b e e
+//        -----
+//         eca
+
+// s2 = l e c a b e e
+//          -----
+//           cab    ← Yes!
