484e2675adc233233a5620368de856218a313d17
Shahmeer Sajid <118081563+ShahmeerSajid@users.noreply.github.com>
2026-07-20 17:05:42 +0500

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = 0;
+
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+            int hours = 0;
+
+            for (int pile : piles) {
+                hours += pile / mid;
+
+                if (pile % mid != 0) {
+                    hours++;
+                }
+            }
+
+            if (hours <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
